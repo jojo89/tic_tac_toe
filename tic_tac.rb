aeada5084ecde601f96dbc59f79695d4d0bf6298
Joseph Burtoft <burtoftallbiz@gmail.com>
2015-05-07 00:11:13 -0400

    solid but dumb ai

--- tic_tac.rb
+++ tic_tac.rb
@@ -1,32 +1,45 @@
 require 'pry'
 class Game
-  attr_accessor :grid, :turn, :finished
- 
-  def initialize(piece, grid_size, turn)
-    @piece = piece
+  attr_accessor :grid, :turn, :finished, :piece, :square_count, :squares_occupied
+
+  def initialize(piece, grid_size)
+    set_piece(piece)
     @grid = make_grid(grid_size)
-    @turn = turn
+    @square_count = grid_size * grid_size
+    @squares_occupied = 0
+    @turn = rand(1..2)
     @finished = false
   end
-  
+
   def character
-    turn == true ? "|x|" : "|o|"
+    turn == 1 ? "|x|" : "|o|"
   end
  
   def finished?
-    finished
+    finished || out_of_squares?
+  end
+  
+  def out_of_squares?
+    if square_count == squares_occupied
+      puts "cats game!"
+      true
+    end
+  end
+  
+  def set_piece(piece)
+    @piece = "|#{piece}|"
   end
  
   def make_grid(size)
-    array = []
+    rows = []
     size.times do |x|
-      arra=[]
+      cells = []
       size.times do |xx|
-        arra << "| |"
+        cells << "| |"
       end
-      array << arra
+      rows << cells
     end
-    array
+    rows
   end
  
   def display_grid
@@ -34,51 +47,91 @@ class Game
   end
   
   def mark_board(x, y)
-    x = x.to_i - 1
-    y = grid.length - y.to_i
-    
+    x = x - 1
+    y = grid.length - y
     unless grid[y][x] == "| |"
-      puts "That spot has already been selected" 
+      puts "That spot has already been selected" if piece == character
       return
     end
     gr = grid
     gr[y][x] = character
     grid = gr
-    cell = gr[y][x]
-    self.finished = true if grid[y + 1] && grid[y + 2] && (grid[y + 1][x] == cell && grid[y + 2][x] == cell)
-    self.finished = true if grid[y] && grid[y] && (grid[y][x + 1] == cell && grid[y][x + 2] == cell)
-    self.finished = true if x != 0 && grid[y] && grid[y] && (grid[y][x - 1] == cell && grid[y][x - 2] == cell)
-    self.finished = true if y != 0 && grid[y - 1] && grid[y - 2] && (grid[y - 1][x] == cell && grid[y - 2][x] == cell)
-    self.finished = true if grid[y + 1] && grid[y + 2] && (grid[y + 1][x + 1] == cell && grid[y + 2][x + 2] == cell)
-    self.finished = true if x != 0 && grid[y + 1] && grid[y + 2] && (grid[y + 1][x - 1] == cell && grid[y + 2][x - 2] == cell)
-    self.finished = true if y != 0 && grid[y - 1] && grid[y - 2] && (grid[y - 1][x + 1] == cell && grid[y - 2][x + 2] == cell)
-    self.finished = true if y != 0 && x != 0 && grid[y - 1] && grid[y - 2] && (grid[y - 1][x - 1] == cell && grid[y - 2][x - 2] == cell)
-    puts "done" if self.finished
-    change_turn
+    if grid[y + 1] && grid[y + 2] && (grid[y + 1][x] == character && grid[y + 2][x] == character)
+      self.finished = true
+      puts 1
+    end
+    if grid[y] && grid[y] && (grid[y][x + 1] == character && grid[y][x + 2] == character)
+      self.finished = true
+      puts 2
+    end
+    if x != 0 && grid[y] && grid[y] && (grid[y][x - 1] == character && grid[y][x - 2] == character)
+      self.finished = true
+      puts 3
+    end
+    if y != 0 && grid[y - 1] && grid[y - 2] && (grid[y - 1][x] == character && grid[y - 2][x] == character)
+      self.finished = true
+      puts 4
+    end
+    if grid[y + 1] && grid[y + 2] && (grid[y + 1][x + 1] == character && grid[y + 2][x + 2] == character)
+       self.finished = true
+       puts 5
+    end
+    if x != 0 && grid[y + 1] && grid[y + 2] && (grid[y + 1][x - 1] == character && grid[y + 2][x - 2] == character)
+      self.finished = true
+      puts 6
+    end
+    if y != 0 && grid[y - 1] && grid[y - 2] && (grid[y - 1][x + 1] == character && grid[y - 2][x + 2] == character)
+      self.finished = true
+      puts 7
+    end
+    if y != 0 && x != 0 && grid[y - 1] && grid[y - 2] && (grid[y - 1][x - 1] == character && grid[y - 2][x - 2] == character)
+      self.finished = true
+      puts 8
+    end
     display_grid
+    puts character + "just won" if finished
+    change_turn
+    self.squares_occupied = squares_occupied + 1
   end
-  
+
   def change_turn
-    turn ? @turn = false : @turn = true
+    @turn == 2 ? @turn = 1 : @turn = 2
   end
 end
  
-game = Game.new("x", 3, true)
- 
-puts "do you want to be exes? y/n"
-input = gets.chomp
- 
-until input == "y" || input == "n"
-  puts "do you want to be exes? y/n"
-  input = gets.chomp
+piece = nil
+grid_size = nil
+
+until piece == "x" || piece == "o"
+  puts "do you want to be exes or ohs? x/o"
+  piece = gets.chomp
 end
- 
+
+until grid_size.to_i > 2
+  puts "How large do you want the grid to be?"
+  grid_size = gets.chomp
+end
+
+game = Game.new(piece, grid_size.to_i)
+x = 0
+y = 0
+
 until game.finished?
-  puts "enter the cordinate of the x axis you would like to place your spot."
-  x = gets.chomp
-  puts "enter the cordinate of the y axis you would like to place your spot."
-  y = gets.chomp
-  game.mark_board(x, y)
+  if game.piece == game.character
+    until x.to_i.between?(1, grid_size.to_i) && y.to_i.between?(1, grid_size.to_i)
+      puts "enter the cordinate of the x axis you would like to place your spot."
+      x = gets.chomp.to_i
+      puts "enter the cordinate of the y axis you would like to place your spot."
+      y = gets.chomp.to_i
+    end
+    game.mark_board(x, y)
+  else
+    x = rand(1..grid_size.to_i)
+    y = rand(1..grid_size.to_i)
+    game.mark_board(x, y)
+  end
+    x=0
+    y=0
 end
- 
+
 puts "Thank you"
